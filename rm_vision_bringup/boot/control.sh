e7ccec1d03b20b5117ca5a33fed38190c2da12f0
wyt <1281974951@qq.com>
2024-05-25 02:08:00 +0800

    2024.5.25 feat:自啓動配置

---
+++ rm_vision_bringup/boot/control.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source /home/wolfvision/workspace/rm_ws/install/setup.bash
+ros2 launch /home/wolfvision/workspace/rm_ws/src/rm_vision/rm_vision_bringup/launch/vision_bringup.launch.py
